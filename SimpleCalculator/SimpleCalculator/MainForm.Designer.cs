c3755da1c01268cdf5e395b2a68551c00d09c5c8
gibsman <gibsman319@gmail.com>
2015-07-06 13:11:48 +0600

    Added interface

--- SimpleCalculator/SimpleCalculator/MainForm.Designer.cs
+++ SimpleCalculator/SimpleCalculator/MainForm.Designer.cs
@@ -28,20 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.FirstArgument = new System.Windows.Forms.TextBox();
+            this.SecondArgument = new System.Windows.Forms.TextBox();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.Addition = new System.Windows.Forms.Button();
+            this.Substraction = new System.Windows.Forms.Button();
+            this.Multiplication = new System.Windows.Forms.Button();
+            this.Division = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // FirstArgument
+            // 
+            this.FirstArgument.Location = new System.Drawing.Point(13, 13);
+            this.FirstArgument.Name = "FirstArgument";
+            this.FirstArgument.Size = new System.Drawing.Size(215, 20);
+            this.FirstArgument.TabIndex = 0;
+            // 
+            // SecondArgument
+            // 
+            this.SecondArgument.Location = new System.Drawing.Point(13, 52);
+            this.SecondArgument.Name = "SecondArgument";
+            this.SecondArgument.Size = new System.Drawing.Size(215, 20);
+            this.SecondArgument.TabIndex = 1;
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(13, 92);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(215, 20);
+            this.Result.TabIndex = 2;
+            // 
+            // Addition
+            // 
+            this.Addition.Location = new System.Drawing.Point(13, 137);
+            this.Addition.Name = "Addition";
+            this.Addition.Size = new System.Drawing.Size(60, 42);
+            this.Addition.TabIndex = 3;
+            this.Addition.Text = "+";
+            this.Addition.UseVisualStyleBackColor = true;
+            // 
+            // Substraction
+            // 
+            this.Substraction.Location = new System.Drawing.Point(79, 137);
+            this.Substraction.Name = "Substraction";
+            this.Substraction.Size = new System.Drawing.Size(60, 42);
+            this.Substraction.TabIndex = 4;
+            this.Substraction.Text = "-";
+            this.Substraction.UseVisualStyleBackColor = true;
+            this.Substraction.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Multiplication
+            // 
+            this.Multiplication.Location = new System.Drawing.Point(145, 137);
+            this.Multiplication.Name = "Multiplication";
+            this.Multiplication.Size = new System.Drawing.Size(60, 42);
+            this.Multiplication.TabIndex = 5;
+            this.Multiplication.Text = "*";
+            this.Multiplication.UseVisualStyleBackColor = true;
+            // 
+            // Division
+            // 
+            this.Division.Location = new System.Drawing.Point(211, 137);
+            this.Division.Name = "Division";
+            this.Division.Size = new System.Drawing.Size(60, 42);
+            this.Division.TabIndex = 6;
+            this.Division.Text = "/";
+            this.Division.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(279, 261);
+            this.Controls.Add(this.Division);
+            this.Controls.Add(this.Multiplication);
+            this.Controls.Add(this.Substraction);
+            this.Controls.Add(this.Addition);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.SecondArgument);
+            this.Controls.Add(this.FirstArgument);
+            this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox FirstArgument;
+        private System.Windows.Forms.TextBox SecondArgument;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Button Addition;
+        private System.Windows.Forms.Button Substraction;
+        private System.Windows.Forms.Button Multiplication;
+        private System.Windows.Forms.Button Division;
+
     }
 }
 
--- SimpleCalculator/SimpleCalculator/MainForm.cs
+++ SimpleCalculator/SimpleCalculator/MainForm.cs
@@ -16,5 +16,15 @@ namespace SimpleCalculator
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
