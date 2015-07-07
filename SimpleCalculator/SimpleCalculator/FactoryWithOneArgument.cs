dedd2328d82bfbc4fa1dcc431d970a1a82d0269b
Kattrisha <katty.s1996@mail.ru>
2015-07-07 13:24:54 +0600

    Added factories

---
+++ SimpleCalculator/SimpleCalculator/FactoryWithOneArgument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public  static class FactoryWithOneArgument
+    {
+        public static IOperationWithOneArgument Calculate(string operation)
+        {
+            switch (operation)
+            {
+                case "Sin": return new Sin();
+                case "Squared": return new Squared();
+                default:throw new Exception("Unknow error!");
+            }
+        }
+
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator/FactoryWithTwoArguments.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public static class FactoryWithTwoArguments
+    {
+        public static IOperationWithTwoArguments Calculate(string operation)
+        {
+            switch (operation)
+            {
+                case "Add": return new Add();
+                case "Substract": return new Substract();
+                case "Divide": return new Divide();
+                case "Multiply": return new Multiply();
+                default: throw new Exception("Unknow error!");
+            }
+        }
+
+    }
+}
