fdbf896931291982f30dae8de66e634bfa6e1146
Kattrisha <katty.s1996@mail.ru>
2015-07-09 12:40:07 +0600

    Added Factory Tests

---
+++ SimpleCalculator/SimpleCalculator.Tests/OneArgumentFactoryTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using SimpleCalculator.OneArgument;
+
+namespace SimpleCalculator.Tests
+{
+    [TestFixture]
+    public class OneArgumentsFactoryTests
+    {
+        [TestCase(typeof(Sin), "Sin")]
+        [TestCase(typeof(Cos), "Cos")]
+        [TestCase(typeof(Tan), "Tan")]
+        [TestCase(typeof(Module), "Module")]
+        [TestCase(typeof(Sqrt), "Sqrt")]
+        [TestCase(typeof(Squared), "Squared")]
+        [TestCase(typeof(Lg), "Lg")]
+        [TestCase(typeof(Ln), "Ln")]
+        [TestCase(typeof(InverseValue), "InverseValue")]
+        [TestCase(typeof(Expanenta), "Expanenta")]
+        [TestCase(typeof(Asin), "Asin")]
+        [TestCase(typeof(Acos), "Acos")]
+
+        public void FactoryTest(Type type, string name)
+        {
+            Type resultType = FactoryWithOneArgument.GetCalculator(name).GetType();
+            Assert.AreEqual(type, resultType);
+        }
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator.Tests/TwoArgumentsFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using SimpleCalculator.TwoArguments;
+
+namespace SimpleCalculator.Tests
+{
+    [TestFixture]
+    public class TwoArgumentsFactoryTests
+    {
+        [TestCase(typeof(Add), "Addition")]
+        [TestCase(typeof(Divide), "Division")]
+        [TestCase(typeof(Substract), "Substraction")]
+        [TestCase(typeof(Multiply), "Multiplication")]
+        [TestCase(typeof(Power), "Power")]
+
+        public void FactoryTest(Type type, string name)
+        {
+            Type resultType = FactoryWithTwoArguments.GetCalculator(name).GetType();
+            Assert.AreEqual(type, resultType);
+        }
+
+    }
+}
